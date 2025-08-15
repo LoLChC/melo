8d9d29115c7d5a9a13df93549f856ddc834745e2
LoLChC <lolchc55@gmail.com>
2025-08-15 16:27:34 +0300

    asdfaa

--- meloapp/settings.py
+++ meloapp/settings.py
@@ -69,7 +69,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "lolchc55@gmail.com"
 EMAIL_HOST_PASSWORD = "uzxc jxut vytz ihbo"
